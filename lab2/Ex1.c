8c0e1abe8f7e0ba33492542ff82a901a04526fe3
kenny0113 <56864424+kenny0113@users.noreply.github.com>
2019-11-08 06:21:05 -0800

    Update Ex1.c

--- lab2/Ex1.c
+++ lab2/Ex1.c
@@ -1,33 +1,23 @@
 #include <stdio.h>
 
 
-int main () {
-    char str[40];
-    int remainder[32];
-    int b,k,i,cnt=0;
-    unsigned int a =-1;
-    k=0;
-	
-// 	输出最大数-1
-    printf("Max value of unsiened int %u\n",a);
-	
-	
-	
-//  转二进制	
-    while (a!=0){
-        b=a/2;
-        k++;
-        remainder[k]=a-b*2;
-        a=a/2;
-    };
-    for (i=k;i>=1;i--){
-	if (remainder[i]==1){
-		cnt++;
-		}
-			       
-    } 
+int main() {
+
+
+	int cnt = 0, i;
+	unsigned int num = -1;
+
+
+	printf("Max value of unsigned int %u\n", num);
 
 	
-    printf("Max bit of unsiened int %d\n",cnt);
-    return 0;
+	while (num != 0) {
+
+		cnt++;  
+		num /= 2; 
+	};
+
+	printf("Max bit of unsigned int %d\n", cnt);
+	system("pause");
+	return 0;
 }

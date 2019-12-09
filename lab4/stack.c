716a82d6091f686284c3ea7dde89609044ee81b1
kenny0113 <56864424+kenny0113@users.noreply.github.com>
2019-12-09 03:39:16 -0800

    Update stack.c

--- lab4/stack.c
+++ lab4/stack.c
@@ -156,11 +156,6 @@ int getop(char s[]) {
 	if (!isdigit(c) && c != '.'&&c !='-')
 		return c;
 	i = 0;
-	if (isdigit(c))
-		while (isdigit(s[++i] = c = getch()));
-	if (c == '.')
-		while (isdigit(s[++i] = c = getch()));
-	    s[i] = '\0';
 	if (c=='-')
 	    if(isdigit(c = getch())|| c =='.')
 		 s[++i]=c;
@@ -169,6 +164,12 @@ int getop(char s[]) {
 			  ungetch(c);
 			return '-';
 		} 
+	if (isdigit(c))
+		while (isdigit(s[++i] = c = getch()));
+	if (c == '.')
+		while (isdigit(s[++i] = c = getch()));
+	    s[i] = '\0';
+	
 	if (c != EOF)
 		ungetch(c);
 

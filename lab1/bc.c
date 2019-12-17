0d48d6e7b979968d5957039eca9c8ede53cbd320
jjt120799 <2602775179@qq.com>
2019-12-16 23:36:09 -0800

    second push

--- lab1/bc.c
+++ lab1/bc.c
@@ -1,13 +1,18 @@
 #include<stdio.h>
-int main(){
-    long long input;
-    int lb = 0;
-    scanf  ("%lld",&input);
-    while(input){
-        if(input % 2==1){
-            lb++;}
-    input /= 2;
+
+int main() {
+    int num;
+    int cout= 0;
+    scanf("%d",&num);
+    if(num<=99999){
+    while(num) {
+        if(num%2==1)cout++;
+        num/=2;
     }
-    printf ("%d\n",lb);
+    printf("%d",cout);}
+    else{
+        printf("number too large");
+    }
+
     return 0;
 }

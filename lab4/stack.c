48325cd28d6ba271edb025bd33ee622800b62f45
jjt120799 <2602775179@qq.com>
2020-01-05 20:48:27 -0800

    new push

--- lab4/stack.c
+++ lab4/stack.c
@@ -7,26 +7,14 @@
 #define MAXVAL 100
 #define BUFSIZE 100
 
-#define Copy 'A'   //要用复制功能在输入字符串时敲A
-#define Print 'B'  //要用打印功能在输入字符串时敲B
-#define Exchange 'C' //要用交换功能在输入字符串时敲C
-#define Clear 'D'  //要用清除功能在输入字符串时敲D
 
 int getop(char[]);
 int getch(void);
 void ungetch(int);
-
 void push(double);
-
 double pop(void);
-
-void PrintStack(); 
-
-void CopyStack(); 
-
-void ExchangeStack(); 
-
-void ClearStack(); 
+void clear(void); 
+double top(void);
 
 int sp = 0;
 double val[MAXVAL];
@@ -37,8 +25,10 @@ main() {
 	
 	
 	int type;
+	double op1;
 	double op2;
 	char s[MAXOP];
+	
 
 	while ((type=getop(s))!=EOF)
 	{
@@ -48,10 +38,8 @@ main() {
 		case NUMBER:
 			push(atof(s));
 			break;
-	
-		case '+':
-	
-			push(pop() + pop());
+	    case '+':
+            push(pop() + pop());
 			break;		
 		case '*':
 			push(pop()*pop());
@@ -74,20 +62,29 @@ main() {
 			else
 				printf("Error: ZERO divisor\n");
 			break;
+		case'p':
+			op2=pop();
+			push(op2);
+		    break;
+		case'c':
+		    clear();
+		    break;
+		case'd':
+		    op2=pop();
+		    push(op2);
+		    push(op2);
+		    break;
+		case's':
+		    op1=pop();
+		    op2=pop();
+		    push(op1);
+		    push(op2);
+		    break;
+		case'o':
+		    for(int i=0;i<sp;i++)
+			 printf("%g\t",val[i]);
 		case '\n':
-			printf("\t%.8g\n", pop());
-			break;
-		case Copy:
-			CopyStack();
-			break;
-		case Print:
-			PrintStack();
-			break;
-		case Exchange:
-			ExchangeStack();
-			break;
-		case Clear:
-			ClearStack();
+			printf("\t%.8g\n", top());
 			break;
 		default:
 			printf("Error: Unknown command %d\n", s);
@@ -96,6 +93,7 @@ main() {
 
 	}
 	return 0;
+
 }
 
 void push(double f)
@@ -114,48 +112,20 @@ double pop(void)
 	return 0.0;
 }
 
-void PrintStack() {
-	int temp = sp;
-	if (temp > 0)
-		printf("Top Stack Element: %.2f\n", val[--temp]);
-	else
-		printf("Error: stack empty\n");
+
+void clear(void)
+{
+	sp=0;
 }
 
-void CopyStack() {
-	
-	int temp = sp;
-
-	if (temp < MAXVAL)
-		val[sp++] = val[--temp];
-	else
-		printf("Error: stack full");
-}
-
-void ExchangeStack() {
-	double temp;
-	if (sp >= 2) {
-		double num1 = val[--sp];
-		double num2 = val[--sp];
-		val[sp++] = num1;
-		val[sp++] = num2;
-	}
-	else
-		printf("Error: Cannot exchange with empty");
-}
-void ClearStack() {
-	if (sp > 0)
-		while (sp) val[--sp] = 0.0;
-	else
-		printf("Error: stack empty");
-}
 int getop(char s[]) {
 	int i, c;
 	while ((s[0] = c = getch()) == ' ' || c == '\t');
 	s[1] = '\0';
+	i=0;
 	if (!isdigit(c) && c != '.'&&c !='-')
 		return c;
-	i = 0;
+	
 	if (c=='-')
 	    if(isdigit(c = getch())|| c =='.')
 		 s[++i]=c;
@@ -187,4 +157,13 @@ void ungetch(int c)
 		printf("ungetch: too many characters\n");
 	else
 		buf[bufp++] = c;
+}
+
+double top(){
+	double op2;
+    op2=pop();
+	push(op2);
+
+	return op2;
+
 }
